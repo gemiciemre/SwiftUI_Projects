f4b66ba0981b0c454788206acbf4f34bf0ff63cb
Emre Gemici <emregemici5@gmail.com>
2023-07-20 21:21:37 +0300

    Africa App Updated

---
+++ 5-Africa App/Africa/Africa/App/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Emre Gemici on 19.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+    let animals : [Animal] = Bundle.main.decode("animals.json")
+    
+    // MARK: - BODY
+    var body: some View {
+        NavigationView{
+            List{
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top:0, leading: 0, bottom: 0,trailing: 0))
+                ForEach(animals) { animal in
+                    NavigationLink(destination: AnimalDetailView(animal: animal)){
+                        AnimalListItemView(animal: animal)
+                    }
+                }//: LOOP
+            }//: LIST
+            .navigationBarTitle("Africa", displayMode: .large)
+        }//: NAVIGATION
+    }
+}
+    // MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 5-Africa App/Africa/Africa/Component/InsetFactView.swift
@@ -0,0 +1,36 @@
+//
+//  InsetFactView.swift
+//  Africa
+//
+//  Created by Emre Gemici on 20.07.2023.
+//
+
+import SwiftUI
+
+struct InsetFactView: View {
+    
+    let animal : Animal
+    
+    var body: some View {
+        GroupBox{
+            TabView{
+                ForEach(animal.fact, id: \.self){ item in
+                    Text(item)
+                }
+            }//:TABS
+            .tabViewStyle(PageTabViewStyle())
+            .frame(minHeight: 148,idealHeight: 168,maxHeight: 180)
+        }//: GROUPBOX
+    }
+}
+
+struct InsetFactView_Previews: PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        InsetFactView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
