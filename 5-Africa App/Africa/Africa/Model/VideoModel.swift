f6c50dcadbf41248d43482a1e986d5e91c13e8e9
Emre Gemici <emregemici5@gmail.com>
2023-07-21 22:47:11 +0300

    Africa App Updated

---
+++ 5-Africa App/Africa/Africa/Model/VideoModel.swift
@@ -0,0 +1,15 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Emre Gemici on 21.07.2023.
+//
+
+import Foundation
+
+struct Video : Codable, Identifiable{
+    let id : String
+    let name : String
+    let headline : String
+}
+
