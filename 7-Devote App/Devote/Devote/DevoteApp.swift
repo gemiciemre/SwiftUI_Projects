9a3e8c2b236b441c01a87775a9804ff8adaf4649
Emre Gemici <emregemici5@gmail.com>
2023-08-04 18:53:08 +0300

    Devote App Commit

---
+++ 7-Devote App/Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Emre Gemici on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
