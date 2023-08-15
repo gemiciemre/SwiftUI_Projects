b071480de5b561251d71fb6c22c137afe7efb390
Emre Gemici <emregemici5@gmail.com>
2023-08-15 23:29:05 +0300

    Devote App Updated

---
+++ 7-Devote App/Devote/Devote/Style/CheckBoxStyleView.swift
@@ -0,0 +1,22 @@
+//
+//  CheckBoxStyleView.swift
+//  Devote
+//
+//  Created by Emre Gemici on 15.08.2023.
+//
+
+import SwiftUI
+
+struct CheckBoxStyleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckBoxStyleView_Previews: PreviewProvider {
+    static var previews: some View {
+        Toggle("Placeholder label", isOn: .constant(true))
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
